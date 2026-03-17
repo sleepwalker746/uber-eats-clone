b94c9ccaf5b0af9d9a7e2785fd241cdb4dd1d751
sleepwalker746 <sleepwalker.8@proton.me>
2026-03-17 08:52:50 +0200

    feat: setup core microservices architecture with Eureka and Gateway

--- auth-service/src/main/java/com/august/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/august/auth/AuthServiceApplication.java
@@ -2,8 +2,11 @@ package com.august.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
